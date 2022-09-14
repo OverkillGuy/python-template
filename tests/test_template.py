530ed709c83d74b571f562c8a002a40c68a2e4c0
Jb DOYON <jb@jiby.tech>
2022-09-14 02:24:08 +0100

    Change testing to use docker-run

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,14 +1,20 @@
 import os
 import subprocess
+import sys
+import tarfile
+from collections import namedtuple
+from io import BytesIO
+from tempfile import TemporaryDirectory
 
 import docker
 import pytest
 from cookiecutter import main as ck
 from cookiecutter.generate import generate_context
 from cookiecutter.prompt import prompt_for_config
-from pytest_cases import parametrize_with_cases
+from pytest_cases import fixture, parametrize, parametrize_with_cases
 
-EXTRA_CONTEXT = {"python_version": "3.10"}
+PYTHON_VERSION = "3.9"
+EXTRA_CONTEXT = {"python_version": PYTHON_VERSION}
 
 
 # The config in cookiecutter.json, once expanded
@@ -21,46 +27,110 @@ DOCKER_CLIENT = docker.from_env()
 DOCKER_DEVIMG_NAME = "python-skeleton-testing"
 
 
+Template = namedtuple("Template", ["path", "context"])
+
+
+def print_docker_build_log(build_log_stream):
+    """Convert a docker build log JSON stream to string"""
+    log_item = next(build_log_stream)
+    log_acc = ""
+    # Exhaust the "stream" key while we aren't erroring
+    while "error" not in log_item:
+        log_acc += log_item["stream"]
+        if "\n" in log_acc:  # TODO: Print separate newline on multiple \n in log_acc
+            print(log_acc)
+            log_acc = ""
+        log_item = next(build_log_stream)
+    # Reached the error: print it and quit
+    print(log_item["error"], file=sys.stderr)
+    print(log_item["errorDetail"], file=sys.stderr)
+
+
+def copy_container_path_out(container, path, destination):
+    """Copy a container's path out into destination/"""
+    tar_stream, stats = container.get_archive(path, encode_stream=True)
+    tar_bytestring = BytesIO()
+    for chunk in tar_stream:
+        tar_bytestring.write(chunk)
+    tar_bytestring.seek(0)  # Rewind the in-memory tar file for data-extraction
+    with tarfile.open(fileobj=tar_bytestring, mode="r") as tar_file:
+        tar_file.extractall(destination)
+
+
+@fixture
+@parametrize(python_version=[PYTHON_VERSION])  # "3.7", "3.8", "3.10",
+def template(python_version):
+    """Template expansion fixture, parametrized by python version"""
+    extra_context = {"python_version": python_version}
+    conf = prompt_for_config(
+        generate_context(extra_context=extra_context), no_input=True
+    )
+    with TemporaryDirectory() as tmp_path:
+        path = expand_template(tmp_path, extra_context)
+        yield Template(path, conf)
+
+
 def python_dev_image(template_path):
     """Build the python image of the template's dockerfile"""
     try:
         img = DOCKER_CLIENT.images.build(
-            path=str(template_path), tag=DOCKER_DEVIMG_NAME, rm=True
+            path=str(template_path),
+            tag=DOCKER_DEVIMG_NAME,
+            # buildargs={"USERID": str(os.getuid())},
+            rm=True,
         )
-    except (docker.errors.BuildError, docker.errors.APIError) as e:
-        pytest.fail(f"Failed to build the main templated Dockerfile: {e}")
+    except (docker.errors.BuildError) as e:
+        print(f"Failed to build the main templated Dockerfile. Build log:")
+        print_docker_build_log(e.build_log)
+        raise e
     return DOCKER_DEVIMG_NAME
 
 
-def expand_template(tmp_path):
+def expand_template(tmp_path, extra_context=EXTRA_CONTEXT):
     """Expand a single template"""
     return ck.cookiecutter(
-        ".", extra_context=EXTRA_CONTEXT, output_dir=tmp_path, no_input=True
+        ".", extra_context=extra_context, output_dir=tmp_path, no_input=True
     )
 
 
-# TODO: Use pytest-cases to parametrize out the python_version etc
-@pytest.fixture
-def template(tmp_path):
-    """Invokes cookiecutter simply without specific arguments"""
-    template_path = expand_template(tmp_path)
-    python_dev_image(template_path)
-    yield template_path
-
-
 def docker_run_devimg(command, workdir, raise_on_nonzero_exitcode=True):
-    """Run the given command in the dev image"""
+    """Run the given command in the dev image
+
+
+    Emulate a docker build + docker run + docker cp via docker-py
+    """
+    python_dev_image(workdir)
     try:
-        return DOCKER_CLIENT.containers.run(
+        container = DOCKER_CLIENT.containers.run(
             image=DOCKER_DEVIMG_NAME,
             command=command,
-            volumes=[f"{workdir}:/app"],
-            working_dir="/app",
+            volumes=[
+                # f"{workdir}:/app",
+                # Named volume mount for ownership
+                f"pyskel-{PYTHON_VERSION}:/caches:rw",
+                # "/etc/passwd:/etc/passwd:ro",
+                # "/etc/groups:/etc/groups:ro",
+            ],
+            # working_dir="/app",
             stdout=True,
             stderr=True,
+            # read_only=False,
+            # user=os.getuid(),  # Avoid root-owned permissions issues
+            # group_add=[os.getgid()],
+            environment={"XDG_CACHE_HOME": "/caches/"},
             # auto_remove=True,
+            detach=True,
         )
-
+        # Block till container completed
+        response = container.wait(timeout=90)
+        exit_code = response["StatusCode"]
+        print(container.logs())
+        if exit_code > 0:
+            pytest.fail(f"Build unsuccessful, container exited {exit_code}")
+        # Successful run: copy data back out for analysis
+        copy_source_path = "/workdir"
+        copy_container_path_out(container, copy_source_path, workdir)
+        return workdir + copy_source_path
     except (
         docker.errors.ContainerError,
         docker.errors.APIError,
@@ -73,49 +143,50 @@ def docker_run_devimg(command, workdir, raise_on_nonzero_exitcode=True):
             )
 
 
-def tests_template_renders_ok(tmp_path):
+def tests_template_renders_ok(template):
     """Checks we can invoke cookiecutter simply without specific arguments"""
-    expand_template(tmp_path)
     pass  # Checking the "template" fixture doesn't fail the test
 
 
 def tests_template_packages_ok(template):
     """Checks we can run poetry build on rendered code to get a binary"""
-    docker_run_devimg(["poetry", "build"], template)
-    assert os.listdir(template + "/dist/"), "Nothing was built!"
+    out_path = docker_run_devimg(["poetry", "build"], template.path)
+    assert os.listdir(out_path + "/dist/"), "Nothing was built!"
 
 
 def tests_template_docs_ok(template):
     """Checks we can run make docson rendered code to get HTML"""
-    docker_run_devimg(["make", "docs"], template)
-    assert os.listdir(template + "/docs/build/html/"), "Docs not built"
+    out_path = docker_run_devimg(["make", "docs"], template.path)
+    assert os.listdir(out_path + "/docs/build/html/"), "Docs not built"
 
 
 def tests_template_makes_ok(template):
     """Checks we can run make on rendered code to get a binary/tests"""
-    docker_run_devimg(["make"], template)
-    assert os.listdir(template + "/dist/"), "Nothing was built!"
+    out_path = docker_run_devimg("make", template.path)
+    assert os.listdir(out_path + "/dist/"), "Nothing was built!"
     assert os.path.isfile(
-        template + "/test_results/results.xml"
+        out_path + "/test_results/results.xml"
     ), "Test results not saved"
     assert os.path.isfile(
-        template + "/test_results/coverage.xml"
+        out_path + "/test_results/coverage.xml"
     ), "Coverage report not saved"
     assert os.path.isfile(
-        template + "/test_results/flake8.txt"
+        out_path + "/test_results/flake8.txt"
     ), "Linter results not saved"
     git_changes_post_make = subprocess.run(
-        ["git", "diff-files", "--quiet"], cwd=template
+        ["git", "status", "--short"],
+        cwd=out_path,
+        capture_output=True,
+        text=True,
     )
     assert (
-        git_changes_post_make.returncode == 0
+        not git_changes_post_make.stdout
     ), "Git found unstaged files after running 'make'"
 
 
-def tests_cli_runs_usage(template):
-    """Runs the generated CLI to get usage info"""
-    logs = docker_run_devimg([DEFAULT_CONF["project_slug"], "--help"], template)
-    assert "usage" in str(logs), "Running CLI entrypoint should show usage"
+def tests_cli_runs_ok(template):
+    """Runs the generated CLI's help works"""
+    docker_run_devimg([DEFAULT_CONF["project_slug"], "--help"], template.path)
 
 
 class CasesDockerBuild:
@@ -133,5 +204,5 @@ class CasesDockerBuild:
 @parametrize_with_cases("make_cmd,img_name", cases=CasesDockerBuild)
 def tests_template_makes_docker_ok(template, make_cmd, img_name):
     """Checks we can build a docker image on rendered code"""
-    subprocess.check_call(make_cmd, cwd=template)
+    subprocess.check_call(make_cmd, cwd=template.path)
     subprocess.check_call(["docker", "image", "rm", img_name])
